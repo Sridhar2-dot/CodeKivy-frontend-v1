72761a4a6898bfd816979069cd232a4e3ce7a70f
Sridhar2-dot <sridhargcpaws@gmail.com>
2026-04-25 03:37:41 +0530

    Update Courses.jsx

--- src/pages/Courses.jsx
+++ src/pages/Courses.jsx
@@ -19,19 +19,6 @@ const Courses = () => {
   const courses = [
     {
       id: 1,
-      title: 'Python Basic',
-      duration: '20 Days',
-      price: 499,
-      image: course1,
-      color: 'from-blue-500 to-cyan-500',
-      level: 'Beginner',
-      students: '200+',
-      about: 'Master the fundamentals of Python programming. Learn variables, data types, loops, functions. Perfect for school students starting their coding journey.',
-      pdfUrl: pythonBasicPdf, // <-- CHANGED
-      registerUrl: 'https://docs.google.com/forms/d/e/1FAIpQLSdMIr3sUbDapsx7FWP6f147prEefoU55VAWmWNh4wQ_baBeow/viewform'
-    },
-    {
-      id: 2,
       title: 'Python Advance',
       duration: '40 Days',
       price: 999,
@@ -44,7 +31,7 @@ const Courses = () => {
       registerUrl: 'https://docs.google.com/forms/d/e/1FAIpQLScEiwTe9NLWH0FaVE5--sz6qP_BXMQfg2g7aLDq91ClCQsT6Q/viewform'
     },
     {
-      id: 3,
+      id: 2,
       title: 'Machine Learning Internship',
       duration: '60 Days',
       price: 2999,
